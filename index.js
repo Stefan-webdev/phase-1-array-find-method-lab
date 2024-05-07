85883093e3d652172388c83dc86332f13380da34
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-05-07 15:02:32 +0930

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "N/A"},
+  {year: "2016", result: "W"},
+  {year: "2016", result: "L"},
+  {year: "2016", result: "L"},
+
+]
+
+function superbowlWin(records) {
+  const win = records.find(record => record.result === "W");
+  return win ? win.year : undefined;
+}
