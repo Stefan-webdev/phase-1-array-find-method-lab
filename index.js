637db353b46f2842defb382000a9f4af8da4a7de
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-08-11 16:40:29 +0930

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const superbowlWin = (records) => {
+    const winRecord = records.find(record => record.result === "W")
+
+    return winRecord ? winRecord.year : undefined;
+}
